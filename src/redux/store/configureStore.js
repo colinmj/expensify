a255749ad759bc5feeb38dc9f02bcaa6b0cc8526
Colin MJ <colinmj@telus.net>
2018-06-17 16:45:31 -0700

    Added logic to check if there are redux devtools

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -4,7 +4,9 @@ import filterReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 import thunk from 'redux-thunk';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 export default () => {
   const store = createStore(
@@ -13,8 +15,10 @@ export default () => {
       filters: filterReducer,
       auth: authReducer
     }),
-    applyMiddleware(thunk)
-    // composeEnhancers(applyMiddleware(thunk))
+
+    typeof composeEnhancers === 'function'
+      ? composeEnhancers(applyMiddleware(thunk))
+      : applyMiddleware(thunk)
   );
   return store;
 };
